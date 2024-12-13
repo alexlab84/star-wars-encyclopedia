7bf78a6cd09af129c38cf53c5f838b71a0fb7068
ale <alexlab84@hotmail.com>
2024-12-13 19:39:06 +0100

    I add integration of the components in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-import { useState, } from 'react';
+import { useEffect, useState, } from 'react';
 import Header from './components/Header';
 import CharacterList from './components/CharacterList';
 import fetchCharacters from './services/api';
+import Pagination from './components/Pagination'; 
 import './styles/App.scss'
 
 type Character = {
@@ -14,18 +15,48 @@ type Character = {
 function App() {
   const [query, setQuery] = useState("");
   const [currentPage, setCurrentPage] = useState(1);
-  const [characters, setCharacters] = useState<Character[]>([]);
+  // characters contiene la lista de personajes obtenida de la API.
+  const [characters, setCharacters] = useState<Character[]>([]); 
+  // totalPages se inicializa a 1 y se actualiza según la estructura de la API.
+  const [totalPages, setTotalPages] = useState(1);
+
+
+ 
   
+
+  // Hook useEffect que se ejecuta cada vez que cambian currentPage o query
+  useEffect(() => {
+     // Llama a fetchCharacters con la página actual y la consulta de búsqueda (query)
+    const fetchData = async () => {
+      const data = await fetchCharacters(currentPage, query);
+      // Actualiza el estado con los resultados devueltos.
+      setCharacters(data);
+      // Actualizamos el total de páginas basándonos en los resultados de la API.
+      setTotalPages(9); // swapi tiene 9 páginas de personajes
+    };
+
+    fetchData();
+  }, [currentPage, query]);
 
   const handleSearch = (query: string) => {
     setQuery(query);
-    setCurrentPage(1);  // Reset to first page on new search
+    setCurrentPage(1);  // Reinicia a la primera página en cada nueva búsqueda
+  };
+
+  const handlePageChange = (page: number) => {
+    setCurrentPage(page);
   };
 
   return (
     <>
-      <h1>Hola</h1>
+      <h1>Star Wars Encyclopedia</h1>
       <Header onSearch={handleSearch} />
+      <CharacterList characters={characters} />
+      <Pagination
+        currentPage={currentPage}
+        totalPages={totalPages}
+        onPageChange={handlePageChange}
+      />
     </>
   )
 }
